6e860f8d3ae6a1e9b0f09d753bf27a13f40d30eb
李民捷 <344831445@qq.com>
2021-05-08 16:22:14 +0800

    初始化

---
+++ server.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const path = require('path')
+const route = require('koa-route')
+const static = require('koa-static')
+const fs = require('fs')
+const app = new Koa()
+const main = ctx => {
+  ctx.response.type = 'html';
+  ctx.response.body = fs.createReadStream('./pages/index.html');
+};
+const staticPath = './pages'
+app.use(route.get('/', main));
+app.use(static(
+  path.join( __dirname,  staticPath)
+))
+
+app.listen(8080)
